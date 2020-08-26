cb93868345e40caea271bf61f39e97059f74f894
LTurns <lizzieturney@gmail.com>
2020-08-26 14:23:04 +0100

    some unit tests for project added

---
+++ src/components/about.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import About from './about';
+import { cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import renderer from "react-test-renderer"
+
+describe('About', () => {
+
+ afterEach(cleanup);
+
+ // you can pass a combination of props and make different snapshots for them - to make sure each variation renders. 
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<About />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('matches snapshot', () => {
+ const tree = renderer.create(<About />).toJSON
+ expect(tree).toMatchSnapshot();
+})
+
+})
+
+// you can use props to check whether your tests are working. Props can be like mocks -
+// you can create a prop constant, then expect your element to have this same content - this way you nknow the prop is working. 
+
+// clean up your tests everytime - using afterEach or beforeEach -
+
